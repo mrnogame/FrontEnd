7b6938b1ce36dd3a1193a368523c45c614e8675a
adam <adam.00228@hotmail.fr>
2023-04-04 09:35:43 +0200

    123

---
+++ train/train.js
@@ -0,0 +1,20 @@
+var a = 43;
+var b = 40;
+var c = 50;
+var z = (a + b);
+function somma(a,b) {
+let z = (a + b);
+}
+console.log('z:', z);
+
+let myArray = [5, 6, 13, 19, 22, 80 ];
+let indexe = myArray.indexe(4);
+console.log(indexe);
+
+
+
+/*  console.log(myArray[3]);
+console.log(myArray[2]);
+console.log(myArray[21]);
+console.log(myArray[3]);
+ */
